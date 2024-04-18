8a718ae0e24c1b2876f6b66115b37e0c02f0c444
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-04-18 11:52:57 +0200

    DateCounter fix

--- Section 16/react-quiz/src/DateCounter.js
+++ Section 16/react-quiz/src/DateCounter.js
@@ -1,8 +1,6 @@
 import { useReducer, useState } from "react";
 
 function reducer(state, action) {
-  console.log(state, action);
-
   switch (action.type) {
     case "dec":
       return { ...state, count: state.count - state.step };
@@ -15,18 +13,9 @@ function reducer(state, action) {
     default:
       return state;
   }
-
-  // if (action.type === "dec") return state - 1;
-  // if (action.type === "inc") return state + 1;
-  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-
-  // const [count, dispatch] = useReducer(reducer, 0);
-
-  // const [step, setStep] = useState(1);
   const initialState = { count: 0, step: 1 };
 
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -44,25 +33,18 @@ function DateCounter() {
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
     dispatch({ type: "inc" });
-
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    // setCount(Number(e.target.value));
     dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
     dispatch({ type: "setStep", payload: Number(e.target.value) });
-    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    // setCount(0);
-    // setStep(1);
     dispatch({ type: "setStep", payload: 1 });
   };
 
