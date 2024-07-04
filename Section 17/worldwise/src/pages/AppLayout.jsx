69957ee39a0692ce1b0d50183927af141026c24e
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-07-04 23:46:28 +0200

    Do 209

---
+++ Section 17/worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppLayout() {
+  return <div>PathLayout</div>;
+}
+
+export default AppLayout;
---
+++ Section 17/worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Homepage</h1>
+      <Link to="/app">App</Link>
+    </div>
+  );
+}
+
+export default Homepage;
