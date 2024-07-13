7d9824a9d6abf525ab38e5bbed69bbe15abec3a5
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-07-13 14:36:03 +0200

    Section 18 finished (Without Challenge)

--- Section 18/worldwise/src/pages/Login.jsx
+++ Section 18/worldwise/src/pages/Login.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 export default function Login() {
+  const a = useAuth();
+  console.log(a.isAuthenticated);
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/app", { replace: true });
+  }, [isAuthenticated, navigate]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
 
   return (
     <main className={styles.login}>
@@ -32,9 +51,9 @@ export default function Login() {
           />
         </div>
 
-        <div>
-          <button>Login</button>
-        </div>
+        <Button type="primary" onClick={handleSubmit}>
+          Login
+        </Button>
       </form>
     </main>
   );
