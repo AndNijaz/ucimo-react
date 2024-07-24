10055a19216e183201820d1fe9d4daab298dc47b
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-07-24 23:30:12 +0200

    commit

--- Section 18/atomic-blog/src/App.js
+++ Section 18/atomic-blog/src/App.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostContext";
+import Test, { SlowComponent } from "./Test";
 
 function createRandomPost() {
   return {
@@ -128,14 +129,20 @@ function List() {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      <Test>
+        <SlowComponent />
+      </Test>
+    </>
   );
 }
 
