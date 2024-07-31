a885802b852dcaffaf6c2f4a9496eeb18fa89add
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-07-31 13:47:15 +0200

    Thunks done

--- Section 20/redux-intro/src/features/account/AccountOperations.js
+++ Section 20/redux-intro/src/features/account/AccountOperations.js
@@ -14,12 +14,13 @@ function AccountOperations() {
     balance,
     loan,
     loanPurpose: accountLaonPurpose,
+    isLoading,
   } = useSelector((store) => store.account);
 
   function handleDeposit() {
     if (depositAmount <= 0) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
   }
 
   function handleWithdrawal() {
@@ -60,7 +61,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : "Deposit"} {depositAmount}
+          </button>
         </div>
 
         <div>
