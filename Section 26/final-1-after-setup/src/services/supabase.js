f849bf9a2f219d22790d8ab860f583cdbabfe44e
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-10-31 21:17:40 +0100

    Finished Supabase

---
+++ Section 26/final-1-after-setup/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ajktfuwbpfbracoatynh.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqa3RmdXdicGZicmFjb2F0eW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzNzQzNTksImV4cCI6MjA0NTk1MDM1OX0.Iw5w-HbT6sx586V2ff7jpCMe_CJAeEddthga1FPBic4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
